e0ec20a2dca631ead3b98d55d53fb296ff5d5bcf
Yibo Cao <cyb0124@uw.edu>
2019-06-24 08:21:49 -0700

    looping forward script

--- server/OCServer/Entry.cpp
+++ server/OCServer/Entry.cpp
@@ -22,6 +22,14 @@ int main() {
     Server server(io, 1847);
     Factory factory(server, 1000, "center", "33d", Actions::up);
     factory.addStorage(std::make_unique<StorageChest>(factory, "center", "33d", Actions::down, Actions::up));
+    factory.addStorage(std::make_unique<StorageDrawer>(factory, "center", "33d", Actions::east, Actions::up, std::vector<SharedItemFilter>{
+      filterLabel("Cobblestone"),
+      filterLabel("Birch Sapling"), filterLabel("Birch Wood"),
+      filterLabel("Oak Sapling"), filterLabel("Oak Wood"), filterLabel("Apple"),
+      filterLabel("Iron Ore Piece")
+    }));
+    factory.addBackup(filterLabel("Oak Sapling"), 8);
+    factory.addBackup(filterLabel("Birch Sapling"), 8);
     factory.start();
     io.io.run();
     return EXIT_SUCCESS;
--- server/forward.sh
+++ server/forward.sh
@@ -1,1 +1,3 @@
-ssh -i ../../lckey/cyb1.pem -N -R :1847:localhost:1847 ec2-user@cyb1.net
+while true; do
+  ssh -i ../../lckey/cyb1.pem -N -R :1847:localhost:1847 ec2-user@cyb1.net
+done
