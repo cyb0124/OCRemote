0d485f52afc548993919beb676a580915a5e4aeb
Yibo Cao <cyb0124@uw.edu>
2019-06-24 19:38:02 -0700

    More robust port forwarding script

--- server/forward.sh
+++ server/forward.sh
@@ -1,3 +1,4 @@
 while true; do
-  ssh -i ../../lckey/cyb1.pem -N -R :1847:localhost:1847 ec2-user@cyb1.net
+  ssh -t -i ../../lckey/cyb1.pem ec2-user@cyb1.net "sudo lsof -n -i :1847 | grep LISTEN | awk '{ print \$2 }' | sort | uniq | xargs kill"
+  ssh -o "ExitOnForwardFailure yes" -i ../../lckey/cyb1.pem -N -R :1847:localhost:1847 ec2-user@cyb1.net
 done
